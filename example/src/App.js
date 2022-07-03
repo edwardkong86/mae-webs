6f19b31c38c70ba0a2cb63911ff0f7477eb5af39
shinghuey27 <57435086+shinghuey27@users.noreply.github.com>
2022-07-04 00:49:43 +0800

    Done 1. Quick Action

--- example/src/App.js
+++ example/src/App.js
@@ -5,14 +5,19 @@ import Dashboard from './demos/layout/Dashboard/Dashboard'
 import Form from './demos/layout/Form/Form'
 import ProductPage from './demos/layout/ProductPageForm/ProductPageForm'
 import ASB_financing from './demos/layout/ProductPage_ASB_financing/ASB_financing-i'
-
+import { QuickActionCard} from 'mae-webs'
+import QRPay from "./assets/QRPay.svg"
 const App = () => {
   return (
     <>
       {/* <Form /> */}
       {/* <Dashboard/> */}
       {/* <ProductPage /> */}
-      <ASB_financing />
+      {/* <ASB_financing /> */}
+      <div style={{padding:"10rem"}}>
+      <QuickActionCard image={QRPay} title="Pay Bills"/>
+
+      </div>
     </>
   )
 }
