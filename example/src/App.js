8dffd74dd3a19fd3848695cbee54b40396d2584b
Edward Kong <edwardkong86@hotmail.com>
2022-11-02 08:58:17 +0800

    CMS List

--- example/src/App.js
+++ example/src/App.js
@@ -7,12 +7,14 @@ import { CardDemo } from './demos/components/CardDemo'
 import { WidgetDemo } from './demos/components/WidgetDemo'
 import DynamoScreen from './screens/DynamoScreen'
 import CMS from './demos/layout/CMS'
+import CMSList from './demos/layout/CMSList'
 
 const App = () => {
   return (
     <>
       {/* <DynamoScreen /> */}
       <CMS />
+      {/* <CMSList /> */}
       {/* <WidgetDemo /> */}
       {/* <ProductPage /> */}
       {/* <ASB_financing /> */}
